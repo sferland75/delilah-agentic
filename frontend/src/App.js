82344a5c2d2444475b856b2485ad75b8b8680444
sferland75 <131406899+sferland75@users.noreply.github.com>
2024-12-06 21:34:54 -0500

    Add React frontend initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClientManagement from './components/ClientManagement';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ClientManagement />
+    </div>
+  );
+}
+
+export default App;
