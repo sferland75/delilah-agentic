b1d28b08c119e9a2632f65704263beceefe9a7a4
sferland75 <131406899+sferland75@users.noreply.github.com>
2024-12-12 13:52:55 -0500

    feat: Add client routes to API router
    - Include client routes in API initialization

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from .assessments import router as assessment_router
+from .clients import router as client_router
 
 api_router = APIRouter()
+
 api_router.include_router(assessment_router, prefix="/assessments", tags=["assessments"])
+api_router.include_router(client_router, prefix="/clients", tags=["clients"])
