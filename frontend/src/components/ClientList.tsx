5f6f4a3550369df3f1e3d83645ce8aedaf631f1c
sferland75 <131406899+sferland75@users.noreply.github.com>
2024-12-20 23:11:26 -0500

    Add frontend auth context and component stubs

---
+++ frontend/src/components/ClientList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ClientList: React.FC = () => {
+  return (
+    <div>Client List Component</div>
+  );
+};
+
+export default ClientList;
---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>Dashboard Component</div>
+  );
+};
+
+export default Dashboard;
