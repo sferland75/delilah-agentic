fbca08ce15d8bd5c6d154e56ff3b3dd7808e3866
sferland75 <131406899+sferland75@users.noreply.github.com>
2025-01-13 15:25:01 -0500

    Add Berg Balance Test values data - Jan 13, 2025

---
+++ backup_jan13_2025/components/FunctionalAssessment/BergBalanceTest/berg-values.ts
@@ -0,0 +1,58 @@
+export const BERG_TEST_ITEMS = [
+  {
+    task: 'Sitting to Standing',
+    instructions: 'Please stand up. Try not to use your hands for support.'
+  },
+  {
+    task: 'Standing Unsupported',
+    instructions: 'Please stand for 2 minutes without holding on.'
+  },
+  {
+    task: 'Sitting Unsupported',
+    instructions: 'Please sit with arms folded for 2 minutes.'
+  },
+  {
+    task: 'Standing to Sitting',
+    instructions: 'Please sit down.'
+  },
+  {
+    task: 'Transfers',
+    instructions: 'Please move from chair to bed and back again. One way toward a chair with armrests and one way toward a chair without armrests.'
+  },
+  {
+    task: 'Standing with Eyes Closed',
+    instructions: 'Please close your eyes and stand still for 10 seconds.'
+  },
+  {
+    task: 'Standing with Feet Together',
+    instructions: 'Place your feet together and stand without holding on.'
+  },
+  {
+    task: 'Reaching Forward with Outstretched Arm',
+    instructions: 'Lift arm to 90 degrees. Stretch out your fingers and reach forward as far as you can.'
+  },
+  {
+    task: 'Retrieving Object from Floor',
+    instructions: 'Pick up the shoe/slipper which is placed in front of your feet.'
+  },
+  {
+    task: 'Turning to Look Behind',
+    instructions: 'Turn to look directly behind you over toward left shoulder. Repeat to the right.'
+  },
+  {
+    task: 'Turning 360 Degrees',
+    instructions: 'Turn completely around in a full circle. Pause. Then turn a full circle in the other direction.'
+  },
+  {
+    task: 'Placing Alternate Foot on Step',
+    instructions: 'Place each foot alternately on the step/stool. Continue until each foot has touched the step/stool four times.'
+  },
+  {
+    task: 'Standing with One Foot in Front',
+    instructions: 'Place one foot directly in front of the other. If you feel that you cannot place your foot directly in front, try to step far enough ahead that the heel of your forward foot is ahead of the toes of the other foot.'
+  },
+  {
+    task: 'Standing on One Leg',
+    instructions: 'Stand on one leg as long as you can without holding.'
+  }
+];
